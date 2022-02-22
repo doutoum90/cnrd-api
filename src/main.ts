f4d8d42e3e2aee22b27bbdf1c6425982787226ec
doutoum90 <doutoummahamat@gmail.com>
2022-02-22 13:15:53 +0100

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'https://cnrd-front.herokuapp.com/home'],
   });
 
-  const option = new DocumentBuilder()
+  /*  const option = new DocumentBuilder()
     .setTitle('CNRD')
     .setDescription("Documentation de l'api")
     .setVersion('1.0')
     .addTag('CNRD')
     .build();
   const docume = SwaggerModule.createDocument(app, option);
-  SwaggerModule.setup('docs', app, docume);
+  SwaggerModule.setup('docs', app, docume); */
   await app.listen(process.env.PORT || 3000);
-
 }
 bootstrap();
